ec3675aa9fda6a3a1f17842a37aabd00cd39794f
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-15 19:36:44 +0530

    Create auth.js

---
+++ server/validators/auth.js
@@ -0,0 +1,17 @@
+const { check }  = require('express-validator');
+
+exports.userRegisterValidator = [
+    check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Name is required'),
+    check('username')
+        .isEmpty()
+        .withMessage('Username is empty'),
+    check('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    check('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be atleast 6 characters long'),
+];
