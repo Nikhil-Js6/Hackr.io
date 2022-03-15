72face7da2af89282543cef5e14185895ac7c3b5
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-15 19:31:12 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
-const { register, activate } = require('../controllers/authController');
+const { register, activate, login } = require('../controllers/authController');
 
-const { userRegisterValidator } = require('../validators/auth');
+const { userRegisterValidator, userLoginValidator } = require('../validators/auth');
 const { runValidation } = require('../validators');
 
 // Routes
-router.post('/register/', userRegisterValidator, runValidation, register);
+
+router.post('/register', userRegisterValidator, runValidation, register);
 router.post('/account/activate', activate);
+router.post('/login', userLoginValidator, runValidation, login);
 
 
 module.exports = router;
