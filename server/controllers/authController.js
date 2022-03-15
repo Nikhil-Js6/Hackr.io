f6b854a04f04c943352843dc2f6324820841ad49
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-15 19:26:14 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const AWS = require("aws-sdk");
+const bcrypt = require("bcrypt");
 const emailParams = require("../util/email");
 const shortid = require('shortid');
 
@@ -26,11 +27,14 @@ class AuthController {
                 });
             }
             console.log(err);
+
+        //Generate hashed password:
+            const hashed_password = bcrypt.hashSync(password, 12);
             
         // Generate JWT:
             const token = jwt.sign(
-                { name, email, password },
-                process.env.JWT_ACCOUNT_ACTIVATION,
+                { name, email, hashed_password },
+                  process.env.JWT_ACCOUNT_ACTIVATION,
                 { expiresIn: '10min' }
             );
 
@@ -56,31 +60,97 @@ class AuthController {
 
     async activate(req, res) {
         const { token } = req.body;
-        console.log(token);
         if (token) {
             jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, data) => {
                 if (err) {
                     return res.status(401).json({
-                        message: 'Invalid link. Plaese try again!'
-                    });
+                        message: 'Invalid link. Please try again!'
+                    })
                 }
-                const { name, email, password } = data;
+                const { name, email, hashed_password } = data;
                 const username = shortid.generate();
 
             // Create new user:
-                const newUser = new User({name, username, email, password});
-                newUser.save((err, user) => {
-                    if (err) {
-                        return res.status(401).json({
-                            message: 'Error saving the user'
+                User.findOne({email}).exec((err, user) => {
+                    if(user){
+                        return res.status(403).json({
+                            message: 'Account is already registered!'
                         })
                     }
-                    res.status(201).json({
-                        message: 'Registration Success. Please login'
+                    const newUser = new User({ name, username, email, hashed_password });
+                    newUser.save((err, user) => {
+                        if (err) {
+                            return res.status(401).json({
+                                message: 'Error saving the user!'
+                            })
+                        }
+                        res.status(201).json({
+                            message: 'Registration Success. Please login'
+                        })
                     })
-                })
+                })  
             });
         }
+    }
+
+    async login(req, res) {
+
+        const { email, password} = req.body;
+        
+        User.findOne({email}).exec((err, user) => {
+
+            if(!user){
+                return res.status(401).json({
+                    message: 'User Email not found!'
+                });
+            }
+            if(err) {
+                return res.status(500).json({
+                    message: 'Something went wrong!'
+                })
+            }
+            
+            const validPassword = bcrypt.compareSync(password, user.hashed_password);
+            if(!validPassword) return res.status(401).json({
+                message: 'Wrong Credentials!'
+            })
+
+            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+            const { _id, name, email, role } = user;
+            return res.status(200).json({ 
+                token,
+                user: {
+                    _id, name, email, role
+                }
+            });
+        });
+
+    // Using try-catch:
+        // try {
+        //     const user = User.findOne({ email });
+        //     if(!user){
+        //         return res.status(401).json({
+        //             message: 'User not found!'
+        //         })
+        //     }
+        //     const validPassword = bcrypt.compareSync(password, user.hashed_password);
+        //     !validPassword && res.status(401).json({
+        //         message: 'Wrong Credentials!'
+        //     })
+
+        //     // Generate tokens
+        //     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        //     // const { name, email, password } = user;
+        //     return res.status(200).json({ 
+        //         token,
+        //         user 
+        //     });
+        // }catch(err) {
+        //     console.log(err);
+        //     res.status(500).json({
+        //         message: 'Something went wrong!'
+        //     })
+        // }
     }
 }
 
