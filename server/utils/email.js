1c1487d113d01e0633e79944bb64d8a7befa2505
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-18 01:54:57 +0530

    Update email.js

--- server/utils/email.js
+++ server/utils/email.js
@@ -25,4 +25,31 @@ const emailParams = (name, email, token) => {
     };
 }
 
-module.exports = emailParams;
+const forgotPasswordEmailParams = (email, token) => {
+    return {
+        Source: process.env.EMAIL_FROM,
+        Destination: {
+            ToAddresses: [email],
+        },
+        ReplyToAddresses: [process.env.EMAIL_TO],
+        Message: {
+            Body: {
+                Html:{
+                    Charset: 'UTF-8',
+                    Data: `<html>
+                                <h1 style="color:#1775e5;">Reset Password:</h1>
+                                <p>Please click the below link to reset your password</p>
+                                <p>${process.env.CLIENT_REQ_URL}/auth/password/reset/${token}</p>
+                                <p>The above link is valid for 10 mins.</p>
+                            </html>`
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'Reset password link.'
+            }
+        }
+    };
+}
+
+module.exports = { emailParams, forgotPasswordEmailParams };
