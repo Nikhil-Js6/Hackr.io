8a2c61acb5e4bdd6942aedf443fb8790ece5e29c
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-17 15:34:07 +0530

    Add files via upload

---
+++ client/next.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    publicRuntimeConfig: {
+        APP_NAME: 'Hacker.io_',
+        API: 'http://localhost:3300/api',
+        PRODUCTION: false,
+        DOMAIN: 'http://localhost:3000',
+    }
+};
