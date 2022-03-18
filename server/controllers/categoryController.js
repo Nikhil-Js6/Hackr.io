83185d5d9514ba97eee38679e6c86d1017b11c48
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-18 23:44:51 +0530

    Create categoryController.js

---
+++ server/controllers/categoryController.js
@@ -0,0 +1,30 @@
+const Category = require('../models/Category');
+const slugify = require('slugify');
+const formidable = require('formidable');
+const { v4 : uuidv4 } = require('uuid');
+const AWS = require('aws-sdk');
+
+class CategoryController {
+
+    async createCategory(req, res) {
+
+    }
+
+    async updateCategory(req, res) {
+
+    }
+
+    async deleteCategory(req, res) {
+
+    }
+    
+    async getCategory(req, res) {
+        
+    }
+
+    async getAllCategories(req, res) {
+
+    }
+}
+
+module.exports = new CategoryController();
