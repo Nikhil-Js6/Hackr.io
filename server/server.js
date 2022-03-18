9925d592a124758cc51a411244fc7ed4f398b6d4
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-19 00:07:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
 const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-const crypto = require('crypto');
 
 const app = express();
 
@@ -12,18 +10,21 @@ app.use(express.json());
 app.use(cors({ origin: '*' }));
 app.use(morgan('dev'));
 
-mongoose.connect('mongodb://localhost:27017/hackr_io');
+mongoose.connect(process.env.CLOUD_DATABASE_URL || process.env.LOCAL_DATABASE_URL);
 
-mongoose.connection.on('connected', ()=>{
+mongoose.connection.on('connected', () => {
     console.log("Database Connected \u{1F525}");
 });
 
 // Routes
 const authRoute = require('./routes/auth');
+const userRoute = require('./routes/user');
+const categoryRoute = require('./routes/category');
 
 // MiddleWares
-
 app.use('/api', authRoute);
+app.use('/api', userRoute);
+app.use('/api', categoryRoute);
 
 let port = process.env.PORT || 3300;
 
