320281c010792fadb53a83511c5b8841b40fdb57
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-23 22:58:08 +0530

    Create dummyData.js

---
+++ client/util/dummyData.js
@@ -0,0 +1,351 @@
+export const Cards = [
+    {
+        url: 'https://www.cloudsavvyit.com/p/uploads/2021/04/075c8694.jpeg?width=1198&trim=1,1&bg-color=000&pad=1,1',
+        name: 'Docker',
+    },
+    {
+        url: 'https://www.mirantis.com/wp-content/uploads/2021/09/k8s-logo-ft-blog-image.png',
+        name: 'Kubernetes',
+    },
+    {
+        url: 'https://hackr.io/tutorials/learn-data-structures-algorithms/logo/logo-data-structures-algorithms?ver=1587721467',
+        name: 'Data structures',
+    },
+    {
+        url: 'https://avatars.mds.yandex.net/get-zen_doc/4498748/pub_605c3f836c8c1e575f10c805_605c3f91109e966efd7f4b60/scale_1200',
+        name: 'Node.js',
+    },
+    {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png',
+        name: 'Angular.js',
+    },
+    {
+        url: 'https://miro.medium.com/max/1000/1*KDMx1YspSrBcFJG-NDZgDg.png',
+        name: 'Next.js',
+    },
+    {
+        url: 'https://pbs.twimg.com/profile_images/1110148780991623201/vlqCsAVP_400x400.png',
+        name: 'Nest.js',
+    },
+    {
+        url: 'https://www.ryadel.com/wp-content/uploads/2017/11/angular-universal-logo-348x215.png',
+        name: 'Universal',
+    },
+    {
+        url: 'https://elasticbeanstalk-ap-southeast-1-805366489044.s3.amazonaws.com/images/uploads/2019/03/30/gatsby.gif' ,
+        name: 'Gatsby.js',
+    },
+    {
+        url: 'https://daqxzxzy8xq3u.cloudfront.net/wp-content/uploads/2019/04/21032431/redux-cover-imgage-1024x768.jpg' ,
+        name: 'Redux.js',
+    },
+    {
+        url: 'https://user-images.githubusercontent.com/7110136/29002857-9e802f08-7ab4-11e7-9c31-604b5d0d0c19.png' ,
+        name: 'Vuex',
+    },
+    {
+        url: 'https://cdn.worldvectorlogo.com/logos/mobx.svg' ,
+        name: 'Mobx.js',
+    },
+    {
+        url: 'https://ngrx.io/assets/images/badge.svg' ,
+        name: 'Ngrx',
+    },
+    {
+        url: 'https://pbs.twimg.com/profile_images/1121395911849062400/7exmJEg4_400x400.png' ,
+        name: 'Svelte.js',
+    },
+    {
+        url: 'https://miro.medium.com/max/1400/1*mh8il3s3hB0Xdpx6ffQLTw.png' ,
+        name: 'Nuxt.js',
+    },
+    {
+        url: 'https://cdn-images-1.medium.com/max/1200/1*FdeBErU2RfeGE51lT0XXfw.png',
+        name: 'Meteor.js',
+    },
+    {
+        url: 'https://cdn.worldvectorlogo.com/logos/dropzone.svg',
+        name: 'Dropzone.js'
+    },
+    {
+        url: 'https://styles.redditmedia.com/t5_u8ynk/styles/communityIcon_s6agobh56o541.png',
+        name: 'Sapper.js',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStK3Yzy5_igR3mwMuHfBmgSbaUrk5QUJpO2Id1KTcv4xV1PG3unZkFK0CF2IqgSK4GH5c&usqp=CAU' ,
+        name: 'Laravel',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoJGt-1CbUlZPgGh_GYwEEipQZ35kO0L344jymj33Bp4t6rqOHc15xhqD2NSj84_eYnMo&usqp=CAU',
+        name: 'Sass',
+    },
+    {
+        url: 'https://mui.com/static/logo.png',
+        name: 'Material UI',
+    },
+    {
+        url: 'https://yt3.ggpht.com/ikv41jMTr1uHGdILrJhvbfVJcDt4oqhwApKX37TjAleF_cRPbF2W-waj7uMnS5JySvnlvAlTCg=s900-c-k-c0x00ffffff-no-rj' ,
+        name: 'Tailwind CSS',
+    },
+    {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg' ,
+        name: 'Bootstrap',
+    },
+    {
+        url: 'https://i.pinimg.com/originals/d1/78/7c/d1787c2575b620e409acdf1fb23a23f7.png' ,
+        name: 'Materialize',
+    },
+    {
+        url: 'https://pbs.twimg.com/profile_images/1384563168144334848/dMn5bLuc_400x400.jpg' ,
+        name: 'Polymer.js',
+    },
+    {
+        url: 'https://miro.medium.com/max/1200/0*L46NIKDigIk65FDR.png' ,
+        name: 'Backbone.js',
+    },
+    {
+        url: 'https://pbs.twimg.com/profile_images/1482078017534967815/dCIbR4nZ_400x400.jpg' ,
+        name: 'Stencil.js',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2bsO5dOGr2vtrd7IzGV1_tr0CrAi8GMzEcnyaaakp2uQUNgx3Lnybks4JJZ9ko5Qvy6Q&usqp=CAU' ,
+        name: 'Vue.js',
+    },
+    {
+        url: 'https://seeklogo.com/images/G/gulp-logo-B221657C5B-seeklogo.com.png' ,
+        name: 'Gulp',
+    },
+    {
+        url: 'https://miro.medium.com/max/1300/1*4BtGcPz3JauG9qsNXzLMXA.gif',
+        name: 'Web Sockets',
+    },
+    {
+        url: 'https://avatars.githubusercontent.com/u/3774533?s=200&v=4',
+        name: 'Hapi.js',
+    },
+    {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/1200px-GraphQL_Logo.svg.png',
+        name: 'GraphQL',
+    },
+    {
+        url: 'https://i.pinimg.com/originals/0e/6c/39/0e6c39f5100af8fb505a02b73add0934.png',
+        name: 'Jamstack',
+    },
+    {
+        url: 'https://assets.browserlondon.com/app/uploads/2019/03/pwa-banner-768x432.png',
+        name: 'PWA',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT92e91iTNYFAfa1t_dzy8djgf5zbrLOutIdVPw_x5Ly7xL3S04CFrCuMXCXgdPSDcO9Bg&usqp=CAU',
+        name: 'Web Assembly',
+    },
+    {
+        url: 'https://nx.dev/documentation/shared/jest-logo.png',
+        name: 'Jest',
+    },
+    {
+        url: 'https://pankod.github.io/superplate/img/enzyme.png' ,
+        name: 'Enzyme',
+    },
+    {
+        url: 'https://digital.ai/sites/default/files/pictures/styles/maxwidth_300/public/pt_logos/jasmine.png?itok=tPrX9h00',
+        name: 'Jasmine',
+    },
+    {
+        url: 'https://digital.ai/sites/default/files/pictures/styles/maxwidth_300/public/pt_logos/karma.png?itok=6rjsUcml',
+        name: 'Karma',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1EJWY-eIByom8qPBqRPYm-rhGcP5a9Mi2eJKohSaMBKm-BQfQ9npv5dUSC_m_L4_s2r8&usqp=CAU',
+        name: 'Webpack',
+    },
+    {
+        url: 'https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_15680ee909406e13c21c8f179f83d99e/gitlab.png',
+        name: 'Gitlab',
+    },
+    {
+        url: 'https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_2ea5f7178028686921161d65d4be481a/bitbucket.png',
+        name: 'Bitbucket',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQanSOyDoqjP2AU-pPCfFOjYeWUYTjfRZZGQS1gL6pK-B-TcZ_-0YotuOnJpw5fWDUz4ys&usqp=CAU' ,
+        name: 'Jira',
+    },
+    {
+        url: 'https://laravelnews.imgix.net/images/rollup1.png?ixlib=php-3.3.1',
+        name: 'Rollup',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpEnxnlqCx62737qFOPPJiGenceX5K-CxEWnxkGg2_2MaUek6kWhMzFfB7z-IzGIW7SPY&usqp=CAU',
+        name: 'Jenkins',
+    },
+    {
+        url: 'https://res.cloudinary.com/practicaldev/image/fetch/s--6UEM9XZf--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/s3uitx6rdv7sod1g2acz.png',
+        name: 'Electron.js',
+    },
+    {
+        url: 'https://webimages.mongodb.com/_com_assets/cms/kuzt9r42or1fxvlq2-Meta_Generic.png',
+        name: 'MongoDB',
+    },
+    {
+        url: 'https://www.softwarecraftsperson.com/images/react-js.jpg',
+        name: 'React.js',
+    },
+    {
+        url: 'https://www.rswebsols.com/wp-content/uploads/2021/04/amazon-web-services-aws-920x613.jpg',
+        name: 'AWS Cloud',
+    },
+    {
+        url: 'https://cdn.dribbble.com/users/528264/screenshots/3140440/media/5f34fd1aa2ebfaf2cd548bafeb021c8f.png?compress=1&resize=400x300&vertical=top',
+        name: 'Firebase',
+    },
+    {
+        url: 'https://cloud.google.com/_static/cloud/images/social-icon-google-cloud-1200-630.png',
+        name: 'Google Cloud',
+    },
+    {
+        url: 'https://yt3.ggpht.com/ytc/AKedOLRt1d4p7bPylasq_66BIC8-k3hkyVjJ2JICQITK=s900-c-k-c0x00ffffff-no-rj',
+        name: 'Flutter',
+    },
+    {
+        url: 'https://pbs.twimg.com/profile_images/1142154201444823041/O6AczwfV_400x400.png',
+        name: 'Go lang',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE5BcIx8RxI7-Ecfj_LZEnHR0Oga558HJQrYETq0YpMPFlTPUgEkbYPcbIf744z2tGATc&usqp=CAU',
+        name: 'Rust',
+    },
+    {
+        url: 'https://cms-assets.tutsplus.com/uploads/users/1499/posts/29328/preview_image/kotlin.jpg',
+        name: 'Kotlin',
+    },
+    {
+        url: 'https://developer.apple.com/swift/images/swift-og.png',
+        name: 'Swift',
+    },
+    {
+        url: 'https://emulationsofttech.files.wordpress.com/2017/10/objective.jpg',
+        name: 'Objective-C',
+    },
+    {
+        url: 'https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png',
+        name: 'C#',
+    },
+    {
+        url: 'https://kreasiteknologidigital.com/wp-content/uploads/2021/02/Yr-pWI4G.jpg',
+        name: 'Dart',
+    },
+    {
+        url: 'https://pbs.twimg.com/profile_images/471641515756769282/RDXWoY7W_400x400.png',
+        name: 'Xamarin',
+    },
+    {
+        url: 'https://www.aalpha.net/wp-content/uploads/2020/01/ionic.png',
+        name: 'Ionic',
+    },
+    {
+        url: 'https://miro.medium.com/max/1200/1*UWCZmBJBUU6pOW4TSuEnqQ.png',
+        name: 'NativeScript',
+    },
+    {
+        url: 'https://www.finsmes.com/wp-content/uploads/2020/08/redis-labs.jpg',
+        name: 'Redis',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_1LgoqZo1dbDcmPK0Bo9SPFykn07O-SFcB7JhDEHHLThBSvnxyK285jfpGkwzj1oxNqA&usqp=CAU',
+        name: 'MySQL',
+    },
+    {
+        url: 'https://download.logo.wine/logo/PostgreSQL/PostgreSQL-Logo.wine.png',
+        name: 'PostgreSQL',
+    },
+    {
+        url: 'https://couchdb.apache.org/image/couch@2x.png',
+        name: 'CouchDB',
+    },
+    {
+        url: 'https://heise.cloudimg.io/width/600/q75.png-lossy-75.webp-lossy-75.foil1/_www-heise-de_/tipps-tricks/imgs/96/2/4/4/2/4/8/2/Bildschirmfoto_2018-05-30_um_08-6aae3c7af906438d.png',
+        name: 'Java',
+    },
+    {
+        url: 'https://preview.redd.it/31b2ii8hchi31.jpg?auto=webp&s=309fe75e96212cf42c4120ca5adedaef52c41e01',
+        name: 'C++',
+    },
+    {
+        url: 'https://images.ctfassets.net/mrop88jh71hl/55rrbZfwMaURHZKAUc5oOW/9e5fe805eb03135b82e962e92169ce6d/python-programming-language.png',
+        name: 'Python',
+    },
+    {
+        url: 'https://html5hive.org/wp-content/uploads/2014/06/js_800x800.jpg',
+        name: 'Javascript',
+    },
+    {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+        name: 'TypeScript',
+    },
+    {
+        url: 'https://img.stackshare.io/service/1012/scala.png',
+        name: 'Scala',
+    },
+    {
+        url: 'https://kinsta.com/wp-content/uploads/2018/05/what-is-php-3-1.png',
+        name: 'Php',
+    },
+    {
+        url: 'https://miro.medium.com/max/600/0*Qpt8ECaPU1BDhOqW.png',
+        name: 'Proton.js',
+    },
+    {
+        url: 'https://cdn.vox-cdn.com/thumbor/YHfKvMFzpSu_j2AY8KoMefG6rTY=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19086219/Android_logo_stacked__RGB_.jpg',
+        name: 'Android',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvgfMrQs2N00ZTP_8eBKbAcaqrdTRWEbWuHEpjM7_hzDWEfL8XS3Ztg94z3ouw8OrJQwI&usqp=CAU',
+        name: 'Linux',
+    },
+    {
+        url: 'https://cdn.icon-icons.com/icons2/2148/PNG/512/terminal_icon_131942.png',
+        name: 'Terminal',
+    },
+    {
+        url: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+        name: 'Github',
+    },
+    {
+        url: 'https://sdtimes.com/wp-content/uploads/2018/05/git_logo.png',
+        name: 'Git Bash',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMxvO0muS52SnD6F_iODQZaRLwHjwpI6d2NUBdMhZfyfetMOuVMVw6pyVgZYS_ix0MFGY&usqp=CAU',
+        name: 'Ubuntu',
+    },
+    {
+        url: 'https://kinsta.com/wp-content/uploads/2018/03/what-is-nginx.png',
+        name: 'Nginx',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE587MjR4b2IYZ5Y-y3cjhpmsTGTNjzwA8xBXtv9EQPBnFD1u2aRWHduqpgT18QXKYHRA&usqp=CAU',
+        name: 'Shell Script',
+    },
+    {
+        url: 'https://digital.ai/sites/default/files/pictures/styles/maxwidth_300/public/pt_logos/portswigger-burp-suite.png?itok=GaUt2UC3',
+        name: 'Burp Suite',
+    },
+    {
+        url: 'https://www.freecodecamp.org/news/content/images/2020/08/wireshark-1.png',
+        name: 'Wireshark',
+    },
+    {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuxQsyds9a8mEaqrjxLlKjVMKaHOm3LYEcRfzj-A9PTpCvni92OcPVaH6KTcMd0_uatLo&usqp=CAU',
+        name: 'BeEF',
+    },
+    {
+        url: 'https://www.cybervie.com/wp-content/uploads/2018/12/metasploit_logo-300x300.png',
+        name: 'Metasploit',
+    },
+    {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Ubuntu_Studio-icon-pd2.png',
+        url1: 'https://static.thenounproject.com/png/637282-200.png',
+        name: 'Others'
+    }
+]
