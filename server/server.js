da39653050aa947f6b3977bd2028b1d625d493d6
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-23 22:40:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(cors({ origin: '*' }));
 app.use(morgan('dev'));
 
