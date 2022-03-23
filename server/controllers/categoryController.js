31044bf34e41de71e3de7b6939025a5d8d3f6d5f
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-03-23 22:39:08 +0530

    Update categoryController.js

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -8,6 +8,45 @@ class CategoryController {
 
     async createCategory(req, res) {
 
+        const { name, content, image } = req.body;
+        const slug = slugify(name);
+
+        const base64Img = new Buffer.from(image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+        const type = image.split(';')[0].split('/')[1];
+
+        const newCategory = new Category({ name, content, slug });
+
+        const bucketParams = {
+            Bucket: 'hackr.io-s3-nikhil',
+            Key: `category/${uuidv4()}${slug}.${type}`,
+            Body: base64Img,
+            ACL: 'public-read',
+            ContentType: `image/${type}`,
+        }
+
+        s3.upload(bucketParams, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({
+                    message: 'Can\'t Upload to s3'
+                });
+            }
+            newCategory.image.url = data.Location;
+            newCategory.image.key = data.Key;
+            newCategory.postedBy = req.user._id;
+
+            newCategory.save((err, data) => {
+                if (err) {
+                    return res.status(400).json({
+                        message: 'Can\'t Create Category!'
+                    })
+                }
+                return res.status(201).json({
+                    message: `${name} Created Successfully!`,
+                    data
+                });
+            });
+        });
     }
     
     async getCategories(req, res) {
